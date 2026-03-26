01222971de2c2b73c6c3dfaf5a32dc6e9a77ef67
praveen <mpraveenyadav72@gmail.com>
2026-03-26 18:00:19 +0530

    multiplication file

---
+++ multiplication.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Take two numbers as input
+echo "Enter first number:"
+read a
+
+echo "Enter second number:"
+read b
+
+# Perform multiplication
+result=$((a * b))
+
+# Display result
+echo "Multiplication result: $result"
